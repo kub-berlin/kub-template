0c73572639d995f4bd5650981790a754fcd25238
Tobias Bengfort <tobias.bengfort@gmx.net>
2015-07-25 22:02:48 +0200

    make error page work (hacky)
    <jdoc:include> does not seem to be available in error.php. I tried to
    use the underlying code to bypass this limitation.
    
    Unfortunately, this commit takes away most of the benefits of the
    partials (see bfc9442).

--- error.php
+++ error.php
@@ -1,15 +1,59 @@
-<?php include('partials/head.php') ?>
+<?php
+defined('_JEXEC') or die;
+
+/**
+ * <jdoc:include /> is not available in this template. So the underlying code
+ * is called directly.
+ */
+
+function jdocIncludeModules($position) {
+    $modules = JModuleHelper::getModules($position);
+    foreach ($modules as $module) {
+        echo JModuleHelper::renderModule($module);
+    }
+}
+
+$config = JFactory::getConfig();
+$doc = JFactory::getDocument();
+$sitename = $config->get('sitename');
+$templatePath = "{$this->baseurl}/templates/{$this->template}";
+
+// clear unneccessary scripts
+$doc->_script = Array();
+$doc->_scripts = Array();
+
+$doc->setTitle($this->error->getCode() . ' - ' . $this->error->getMessage());
+$doc->_metaTags['standard']['viewport'] = 'width=device-width, initial-scale=1.0';
+$doc->_metaTags['http-equiv']['X-UA-Compatible'] = 'IE=edge,chrome=1';
+$doc->_styleSheets["$templatePath/css/kub.css"] = null;
+
+?>
+<!DOCTYPE html>
+<html
+    xmlns="http://www.w3.org/1999/xhtml"
+    xml:lang="<?php echo $this->language; ?>"
+    lang="<?php echo $this->language; ?>"
+    dir="<?php echo $this->direction; ?>"
+    prefix="s: http://schema.org/">
+<head>
+    <meta charset="utf-8" />
+    <?php echo $doc->loadRenderer('head')->render(null) ?>
+    <!--[if lt IE 9]>
+        <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+</head>
+<body>
     <header>
         <a href="/" class="brand-link"><span class="brand-title" role="banner"><?php echo $sitename; ?></span></a>
-        <jdoc:include type="modules" name="header" />
+        <?php jdocIncludeModules('header') ?>
     </header>
     <nav>
-        <jdoc:include type="modules" name="navigation" />
+        <?php jdocIncludeModules('navigation') ?>
     </nav>
     <main>
-        <h1><?php echo JText::_('404 Not Found') ?></h1>
+        <h1><?php echo $this->error->getCode(); ?> - <?php echo JText::_($this->error->getMessage()); ?></h1>
     </main>
     <aside>
-        <jdoc:include type="modules" name="aside" />
+        <?php jdocIncludeModules('aside') ?>
     </aside>
 <?php include('partials/foot.php') ?>
