8ce8f1f709bd00a6f8569e70cfb1c54ef25b0712
Tobias Bengfort <tobias.bengfort@gmx.net>
2016-04-08 23:18:06 +0200

    update nav js

--- js/nav.js
+++ js/nav.js
@@ -1,10 +1,18 @@
-(function() {
-    $(document).ready(function() {
-        $('<a class="nav-toggle">')
-            .append('<i class="fa fa-bars">')
-            .click(function() {
-                $('html').toggleClass('show-nav');
-            })
-            .appendTo('nav');
+window.addEventListener('load', function() {
+    var header = document.getElementById('header');
+    var nav = document.getElementById('nav');
+
+    var wrapper = document.createElement('div');
+    wrapper.innerHTML = '<a class="menu-button" href="#"></a>';
+
+    var menuButton = wrapper.children[0];
+    menuButton.innerHTML = '<i class="burger-icon"></i>';
+
+    menuButton.addEventListener('click', function(event) {
+        event.preventDefault();
+        nav.classList.toggle('is-visible');
+        menuButton.classList.toggle('is-active');
     });
-})();
+
+    header.appendChild(menuButton);
+});
