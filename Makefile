f5a18eb7089dc7bb1763d1ab739c0b8acf11129d
Tobias Bengfort <tobias.bengfort@gmx.net>
2016-06-02 16:56:29 +0200

    bidirectional

--- Makefile
+++ Makefile
@@ -1,2 +1,9 @@
-css/kub.css: css/kub.scss css/_*.scss css/components/_*.scss
-	node-sass --source-map css/kub.css.map -o css $<
+css/kub-rtl.css: css/kub-ltr.css
+	cp $< $@
+	sed -i 's/left/RIGHT/g' $@
+	sed -i 's/right/left/g' $@
+	sed -i 's/RIGHT/right/g' $@
+
+css/kub-ltr.css: css/kub.scss css/_*.scss css/components/_*.scss
+	node-sass --source-map css/kub-ltr.css.map -o css $<
+	mv css/kub.css $@
--- partials/head.php
+++ partials/head.php
@@ -5,6 +5,7 @@ $config = JFactory::getConfig();
 $sitename = $config->get('sitename');
 $templateUrl = "{$this->baseurl}/templates/{$this->template}";
 $templatePath = JPATH_BASE . "/templates/{$this->template}";
+$dir = $this->direction;
 
 include_once "$templatePath/functions.php";
 
@@ -16,7 +17,7 @@ $this->setTitle("$sitename - {$this->getTitle()}");
 $this->setMetaData('viewport', 'width=device-width, initial-scale=1.0');
 $this->setMetaData('X-UA-Compatible', 'IE=edge,chrome=1', True);
 $this->addStyleSheet("$templateUrl/bower_components/normalize-css/normalize.css");
-$this->addStyleSheet("$templateUrl/css/kub.css");
+$this->addStyleSheet("$templateUrl/css/kub-$dir.css");
 $this->addScript("$templateUrl/js/nav.js");
 
 kub_opengraph($this);
@@ -27,7 +28,7 @@ kub_opengraph($this);
     xmlns="http://www.w3.org/1999/xhtml"
     xml:lang="<?php e($this->language); ?>"
     lang="<?php e($this->language); ?>"
-    dir="<?php e($this->direction); ?>"
+    dir="<?php e($dir); ?>"
     prefix="s: http://schema.org/ og: http://ogp.me/ns#">
 <head>
     <meta charset="utf-8">
