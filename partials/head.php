6462a4aba6ab4c9423f5619205e48688e1d8deb5
Tobias Bengfort <tobias.bengfort@gmx.net>
2016-11-12 10:54:03 +0100

    effectively remove plugin scripts/styles

--- partials/head.php
+++ partials/head.php
@@ -1,27 +1,38 @@
 <?php
 defined('_JEXEC') or die;
 
-$config = JFactory::getConfig();
-$sitename = $config->get('sitename');
-$templateUrl = "{$this->baseurl}/templates/{$this->template}";
+$app = JFactory::getApplication();
 $templatePath = JPATH_BASE . "/templates/{$this->template}";
 $dir = $this->direction;
 
 include_once "$templatePath/functions.php";
 
-// clear unneccessary scripts
-$this->_script = Array();
-$this->_scripts = Array();
+function setKubHead() {
+    $doc = JFactory::getDocument();
+    $config = JFactory::getConfig();
 
-$this->setTitle("$sitename - {$this->getTitle()}");
-$this->setMetaData('viewport', 'width=device-width, initial-scale=1.0');
-$this->setMetaData('X-UA-Compatible', 'IE=edge,chrome=1', True);
-$this->addStyleSheet("$templateUrl/bower_components/normalize-css/normalize.css");
-$this->addStyleSheet("$templateUrl/css/kub-$dir.css");
-$this->addScript("$templateUrl/js/nav.js");
-$this->addScript("$templateUrl/js/table.js");
+    $sitename = $config->get('sitename');
+    $templateUrl = "{$doc->baseurl}/templates/{$doc->template}";
+    $dir = $doc->direction;
 
-kub_opengraph($this);
+    // clear unneccessary scripts
+    $doc->_script = Array();
+    $doc->_scripts = Array();
+    $doc->_style = Array();
+    $doc->_styleSheets = Array();
+
+    $doc->setTitle("$sitename - {$doc->getTitle()}");
+    $doc->setMetaData('viewport', 'width=device-width, initial-scale=1.0');
+    $doc->setMetaData('X-UA-Compatible', 'IE=edge,chrome=1', True);
+    $doc->addStyleSheet("$templateUrl/bower_components/normalize-css/normalize.css");
+    $doc->addStyleSheet("$templateUrl/css/kub-$dir.css");
+    $doc->addScript("$templateUrl/js/nav.js");
+    $doc->addScript("$templateUrl/js/table.js");
+
+    kub_opengraph($doc);
+}
+
+$app->registerEvent('onBeforeCompileHead', 'setKubHead');
 
 ?>
 <!DOCTYPE html>
