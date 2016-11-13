6995756fe5767ca15012e46348835d03c45e1f08
Tobias Bengfort <tobias.bengfort@gmx.net>
2016-11-13 09:54:35 +0100

    add footer2 slot

--- partials/foot.php
+++ partials/foot.php
@@ -1,4 +1,8 @@
-    <?php kub_module('footer', 'footer'); ?>
+
+    <footer id="footer">
+        <?php kub_module('footer1'); ?>
+        <?php kub_module('footer2'); ?>
+    </footer>
 
     <?php kub_module('debug'); ?>
 </body>
