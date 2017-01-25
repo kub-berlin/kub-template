488fbecc65d05c7bc4e2fcffee0f899a6cecd376
Tobias Bengfort <tobias.bengfort@posteo.de>
2017-01-25 22:54:31 +0100

    Fix text directon on home

--- partials/home.php
+++ partials/home.php
@@ -14,20 +14,20 @@
         </div>
     </div>
     <address class="homeRow-address">
-        <strong property="name">Kontakt- und Beratungsstelle für Flüchtlinge und Migrant_innen e.V.</strong>
+        <strong property="name"><bdi>Kontakt- und Beratungsstelle für Flüchtlinge und Migrant_innen e.V.</bdi></strong>
         <p property="address" typeof="PostalAddress">
-            <span property="streetAddress">Oranienstr. 159</span><br />
-            <span property="postalCode">10969</span> <span property="addressLocality">Berlin-Kreuzberg</span>
+            <bdi property="streetAddress">Oranienstr. 159</bdi><br />
+            <bdi><span property="postalCode">10969</span> <span property="addressLocality">Berlin-Kreuzberg</span></bdi>
         </p>
         <p>
-            <?php echo JText::_("TPL_KUB_TELEPHONE"); ?>: <a href="tel:+49-30-614-94-00" property="telephone">030 / 614 94 00</a><br />
-            <?php echo JText::_("TPL_KUB_FAX"); ?>: <span property="faxNumber">030 / 615 45 34</span><br />
+            <?php echo JText::_("TPL_KUB_TELEPHONE"); ?>: <a href="tel:+49-30-614-94-00" property="telephone"><bdi>030 / 614 94 00</bdi></a><br />
+            <?php echo JText::_("TPL_KUB_FAX"); ?>: <bdi property="faxNumber">030 / 615 45 34</bdi><br />
             <?php echo JText::_("TPL_KUB_EMAIL"); ?>:  <a property="email" href="mailto:kontakt@kub-berlin.org">kontakt@kub-berlin.org</a><br />
-            <a href="<?php e($doc->baseurl); ?>/images/kub-pubkey.asc" target="_blank">Public PGP-Key</a>
+            <a href="<?php e($doc->baseurl); ?>/images/kub-pubkey.asc" target="_blank"><bdi>Public PGP-Key</bdi></a>
         </p>
         <p>
-            <?php echo JText::_("TPL_KUB_SUBWAY"); ?>: U8 Mortizplatz<br />
-            <?php echo JText::_("TPL_KUB_BUS"); ?>: M29 Moritzplatz
+            <?php echo JText::_("TPL_KUB_SUBWAY"); ?>: <bdi>U8 Mortizplatz</bdi><br />
+            <?php echo JText::_("TPL_KUB_BUS"); ?>: <bdi>M29 Moritzplatz</bdi>
         </p>
         <div property="openingHours" content="Mo,Tu,Th,Fr 09:00-19:00,We 14:00-19:00">
             <jdoc:include type="modules" name="opening-hours" />
