315f668fbf2d8722958dea093c2c14da92cf9186
Tobias Bengfort <tobias.bengfort@posteo.de>
2018-07-07 08:11:21 +0200

    rm legacy newsletter monkeypatch

--- js/newsletter.js
+++
@@ -1,7 +0,0 @@
-// bypass the default javascript
-setTimeout(function() {
-    window.submitjnewsmod1 = function(name) {
-        var form = document.getElementsByName(name)[0];
-        form.submit();
-    };
-}, 100);
