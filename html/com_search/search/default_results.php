37a3c34f26a80ccbdeb04684a3be241aede75368
Tobias Bengfort <tobias.bengfort@posteo.de>
2018-11-02 13:24:03 +0100

    update search template to 3.9.0

--- html/com_search/search/default_results.php
+++ html/com_search/search/default_results.php
@@ -3,23 +3,30 @@
  * @package     Joomla.Site
  * @subpackage  com_search
  *
- * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ *
+ * duplicated from components/com_search/views/search/tmpl/default_results.php
  */
 
 defined('_JEXEC') or die;
+
 ?>
 
 <ul class="search-results<?php echo $this->pageclass_sfx; ?>">
 <?php foreach ($this->results as $result) : ?>
 	<li>
 		<h3 class="result-title">
-			<?php if ($result->href) :?>
-				<a href="<?php echo JRoute::_($result->href); ?>"<?php if ($result->browsernav == 1) :?> target="_blank"<?php endif;?>>
-					<?php echo $this->escape($result->title);?>
+			<?php if ($result->href) : ?>
+				<a href="<?php echo JRoute::_($result->href); ?>"<?php if ($result->browsernav == 1) : ?> target="_blank"<?php endif; ?>>
+					<?php // $result->title should not be escaped in this case, as it may ?>
+					<?php // contain span HTML tags wrapping the searched terms, if present ?>
+					<?php // in the title. ?>
+					<?php echo $result->title; ?>
 				</a>
-			<?php else:?>
-				<?php echo $this->escape($result->title);?>
+			<?php else : ?>
+				<?php // see above comment: do not escape $result->title ?>
+				<?php echo $result->title; ?>
 			<?php endif; ?>
 		</h3>
 		<?php if ($result->section) : ?>
@@ -38,7 +45,6 @@ defined('_JEXEC') or die;
 	</li>
 <?php endforeach; ?>
 </ul>
-
 <div class="pagination">
 	<?php echo $this->pagination->getPagesLinks(); ?>
 </div>
